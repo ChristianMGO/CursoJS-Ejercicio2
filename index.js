16eadd32bfcfb846124bccb870d2d24c736cf837
ChristianMGO <53414839+ChristianMGO@users.noreply.github.com>
2022-07-30 16:56:37 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const lista = [
+  "Christian",
+  25,
+  true,
+  new Date(1996, 10, 06),
+  {
+    titulo: "El libro troll",
+    autor: "Rubén Doblas Gundersen",
+    fecha: new Date(2014, 4, 27),
+    url: "https://www.amazon.es/El-libro-troll-Fuera-Colecci%C3%B3n/dp/8499983197",
+  },
+];
